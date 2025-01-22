2f81c074c254488d59cb66bf3563aeb37a0ede9c
ChiefAnthony <anthonystudie@gmail.com>
2025-01-22 12:09:52 +0100

    add vault close

--- programs/anchor-vault/src/lib.rs
+++ programs/anchor-vault/src/lib.rs
@@ -26,6 +26,12 @@ pub mod anchor_vault {
 
         Ok(())
     }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -153,6 +159,31 @@ pub struct Close<'info> {
     )]
     pub vault_state: Account<'info, VaultState>,
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, self.vault.lamports())?;
+
+        Ok(())
+    }
 }
 
 #[account]
